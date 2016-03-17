3655fc3baed91b0b3fdc9dec52385d9633eafb17
Stephen Ball <stephen@nudgecrm.com>
2016-03-17 14:26:07 +0000

    Install additional packages

--- setup-web.sh
+++ setup-web.sh
@@ -14,9 +14,11 @@ rm dotdeb.gpg
 
 mkdir /var/www
 apt-get upgrade -y
-apt-get install -y git curl php5-cli php5-memcache php5-curl php5-gd php5-mcrypt php5-fpm nginx-full
+apt-get install -y git curl php5-readline php5-cli php5-memcache php5-curl php5-mysqlnd php5-gd php5-fpm nginx-full memcached redis-server
 
-apt-get autoremove -y
+apt-get install -y build-essential
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+apt-get install -y nodejs
 
 curl -sS https://getcomposer.org/installer | php
 chmod +x composer.phar
@@ -24,10 +26,18 @@ mv composer.phar /usr/local/bin/composer
 useradd -m -d /home/deploy -s /bin/bash deploy
 chown deploy:deploy /var/www
 
+apt-get autoremove -y
 apt-get upgrade -y
 rm -rf /var/www/*
 
 mkdir -p /home/deploy/.ssh
+mkdir -p /home/deploy/.composer
+
+# echo '{
+#     "github-oauth": {
+#         "github.com": ""
+#     }
+# }' > /home/deploy/.composer/auth.json
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCasyO1qNW+Z331PzHBQ2sgVuvNKZnpUIzz2F+bhS31q2/b2AaXYcU8ljtW2yVcMlUvrvDkRQyynLza2QBfsXTeJpqtKxujqcLXpRN9t81OLjhKebRP/lExq9I6c4xEnwFBx/OqB7ighDNUZc6zRi80V1K3iloGn12ywpL7vI/+EO+ABXP4sTchwh47bppcBNy4HjOre+NqpLNZkZ02E4lngSaOCY6r36TdICaigeQX6n/Xgwm2rRkr0qNIZsd/IoyLYS6/CWUUJjX16qxXt1wwMiwwpRbZ2IULnZ0lI74QXjucD+Ow0OKwWwgLsN55VUGXVOlpX1GJ2p5mZ3H6YX0B deploy@deployer" > /home/deploy/.ssh/authorized_keys
 
