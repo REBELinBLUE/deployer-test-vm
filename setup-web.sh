5de69dba8bd613167133cb48703a076f82caa735
Stephen Ball <stephen@rebelinblue.com>
2016-03-17 20:44:19 +0000

    Move composer config

--- setup-web.sh
+++ setup-web.sh
@@ -33,7 +33,7 @@ rm -rf /var/www/*
 mkdir -p /home/deploy/.ssh
 mkdir -p /home/deploy/.composer
 
-mv ~/auth.json /home/deploy/.composer/auth.json
+mv /home/vagrant/auth.json /home/deploy/.composer/auth.json
 
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCasyO1qNW+Z331PzHBQ2sgVuvNKZnpUIzz2F+bhS31q2/b2AaXYcU8ljtW2yVcMlUvrvDkRQyynLza2QBfsXTeJpqtKxujqcLXpRN9t81OLjhKebRP/lExq9I6c4xEnwFBx/OqB7ighDNUZc6zRi80V1K3iloGn12ywpL7vI/+EO+ABXP4sTchwh47bppcBNy4HjOre+NqpLNZkZ02E4lngSaOCY6r36TdICaigeQX6n/Xgwm2rRkr0qNIZsd/IoyLYS6/CWUUJjX16qxXt1wwMiwwpRbZ2IULnZ0lI74QXjucD+Ow0OKwWwgLsN55VUGXVOlpX1GJ2p5mZ3H6YX0B deploy@deployer" > /home/deploy/.ssh/authorized_keys
 
